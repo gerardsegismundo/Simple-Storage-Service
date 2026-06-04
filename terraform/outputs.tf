e8c839da0ef2822a797f70937593ebea0e8356f1
Gerard Segismundo <gerardmartinsegismundo@protonmail.com>
2026-06-04 12:14:25 -0400

    c a

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,9 +6,9 @@ output "replica_bucket" {
   value = aws_s3_bucket.replica.id
 }
 
-output "access_point" {
-  value = aws_s3_access_point.primary.arn
-}
+# output "access_point" {
+#   value = aws_s3_access_point.primary.arn
+# }
 
 output "lambda_function" {
   value = aws_lambda_function.processor.function_name
