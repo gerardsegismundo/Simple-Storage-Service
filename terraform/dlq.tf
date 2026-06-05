e26738d2e2ec282580ed412322e93bc9d12bc1e5
Gerard Segismundo <gerardmartinsegismundo@protonmail.com>
2026-06-05 10:27:45 -0400

    added monitoring

--- terraform/dlq.tf
+++ terraform/dlq.tf
@@ -7,3 +7,20 @@ resource "aws_lambda_event_source_mapping" "dlq" {
   event_source_arn = aws_sqs_queue.dlq.arn
   function_name    = aws_lambda_function.processor.arn
 }
+
+resource "aws_cloudwatch_metric_alarm" "dlq_errors" {
+  alarm_name          = "${var.project_name}-dlq-errors"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "ApproximateNumberOfMessagesVisible"
+  namespace           = "AWS/SQS"
+  period              = "300"
+  statistic           = "Sum"
+  threshold           = "1"
+  alarm_description   = "DLQ has messages - Lambda processing failures detected"
+  alarm_actions       = [] # Add SNS topic ARN if you have one
+
+  dimensions = {
+    QueueName = aws_sqs_queue.dlq.name
+  }
+}
